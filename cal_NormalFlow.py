ab33dba7e4bb53670d676c5cea857ef7c9e85bd7
Yosuke Horie(堀江　陽介) <a6988@n-koei.co.jp>
2020-10-30 17:49:28 +0900

    平常流量を計算する関数を作成
    あっているかどうかは、もう少しチェックする必要あり。

---
+++ cal_NormalFlow.py
@@ -0,0 +1,104 @@
+import matplotlib.pyplot as plt
+import pandas as pd
+import datetime as dt
+
+def getTargetFlow(flowFilename:str, thisUsecols:str,
+        startDayStr:str, endDayStr:str):
+    '''
+    使用する流量のdataFrameを返す
+    '''
+    flowData = pd.read_excel(flowFilename,usecols=thisUsecols)
+    # Dateの作成
+    ## 何故かapply関数の中では整数が少数に変換されてしまうので、intを付ける
+    makeDate = lambda thisRow : dt.datetime(
+            int(thisRow.年),int(thisRow.月),int(thisRow.日), int(thisRow.時間))
+    flowData['Date'] = flowData.apply(makeDate,axis=1)
+    ## indexをdateにする
+    flowData.index=flowData['Date']
+    flowData.drop(['年','月','日','時間','Date'],inplace=True,axis=1)
+
+    # 対象期間のみに限定
+    startDay = dt.datetime.strptime(startDayStr, '%Y/%m/%d')
+    endDay = dt.datetime.strptime(endDayStr, '%Y/%m/%d')
+    ## queryメソッド内で変数を使う時は@を付与する
+    targetFlow = flowData.query(" index >= @startDay and index <= @endDay")
+
+    return targetFlow
+
+def calStdFlow(targetFlow, HeisuiOrHousui,makeFlowGraphFlag ):
+    '''
+    平水/豊水流量を算出する
+    '''
+    # 日平均流量の作成
+    dayFlow = targetFlow.resample('D').mean()
+
+    # 平水/豊水で日々を変更
+    heisuiHousuiDays = {"Heisui":185,"Housui":95}
+
+    try:
+        stdDay = heisuiHousuiDays[HeisuiOrHousui]
+    except:
+        print('Heisui(平水流量)かHousui(豊水流量)で指定してください')
+
+    stdFlows = {}
+    # 河川毎に実行
+
+    for thisRiver in dayFlow.columns:
+
+        thisFlow = dayFlow[thisRiver]
+
+        # 平水流量は一年を通じて185日はこれを下回らない流量なので、
+        # 上から並べて185番目の流量となる
+        sortedFlow = thisFlow.sort_values(ascending=False)
+        sortedFlow.index = range(1,len(sortedFlow)+1)
+
+        print("{}:{}".format(thisRiver, round(sortedFlow[stdDay],4)))
+        stdFlows[thisRiver] = round(sortedFlow[stdDay],4)
+
+        if makeFlowGraphFlag == True:
+            makeFlowGraph(thisRiver, sortedFlow, stdDay)
+
+    return stdFlows
+
+    
+def makeFlowGraph(thisRiver, sortedFlow,stdDay):
+    '''
+    日流量を大きい順に並べ変えたグラフを作る
+    '''
+    flowmin = sortedFlow.iloc[0]
+    flowmax = sortedFlow.iloc[-1]
+    fig = plt.figure()
+    ax = fig.add_subplot()
+    ax.plot(sortedFlow)
+    ax.vlines([stdDay],flowmin,flowmax,ls="--",lw=3,color="red")
+    ax.set_title(thisRiver)
+    ax.grid()
+    fig.savefig('./res/flow/{}.png'.format(thisRiver),bbox_inches='tight')
+    plt.clf()
+
+    return 
+
+if __name__ == '__main__':
+    
+    
+
+    flowFilename = './data/〇河川流量データ_解析フォーマット対応.xlsx'
+
+    thisUsecols='A:N' # 入間川は含めていない
+
+    HeisuiOrHousui = 'Heisui'
+    makeFlowGraphFlag = True
+    startDayStr = '2016/4/1'
+    endDayStr = '2017/3/31'
+
+    targetFlow = getTargetFlow(flowFilename,thisUsecols, startDayStr, endDayStr)
+
+    
+
+    stdFlows = calStdFlow(targetFlow, HeisuiOrHousui, makeFlowGraphFlag)
+    
+
+
+
+
+
